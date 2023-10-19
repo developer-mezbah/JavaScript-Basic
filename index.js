7cce7863d8639afb127e0e32887ff34c6838bf65
developer.mezbah <developer.mezbah@gmail.com>
2023-10-19 10:55:04 +0600

    javascript arithmatic, Data type and Date added

---
+++ index.js
@@ -0,0 +1,45 @@
+// Addition
+var addintion = 10 + 10;
+// Subtraction
+var substraction = 10 - 10;
+
+console.log("addintion", addintion, "substarction", substraction);
+
+// Multiplication
+var multiplication = 10 * 10;
+
+// Division
+var divistion = 10 / 10;
+// Increment
+addintion++;
+// Decrement
+substraction--;
+
+console.log(
+  "Multiplication",
+  multiplication,
+  "Divition",
+  divistion,
+  "decrement",
+  substraction,
+  "increment",
+  addintion
+);
+
+// This is string
+var string = "100";
+
+// String convert number
+var number = parseInt(string);
+
+const floatNumber = 12.00000000000;
+
+// Delete the two digits after the decimal point
+console.log("Float", floatNumber.toFixed(2))
+
+console.log("Numaric Data type", typeof(number), number);
+console.log("Boolian Data type", typeof(true, false));
+
+
+// Bd time
+console.log("Current time", new Date())
